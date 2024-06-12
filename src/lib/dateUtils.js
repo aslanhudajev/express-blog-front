3cfffad560c07b60b83b243806e31ce6bf449c98
Aslan <aslambek.hudajev@gmail.com>
2024-06-12 13:49:28 +0200

    Optimized and fixed stuff. Made articles more readable.

--- src/lib/dateUtils.js
+++ src/lib/dateUtils.js
@@ -2,6 +2,5 @@ import { DateTime } from "luxon";
 
 export const prettifyDate = (jsDate) => {
   const date = new Date(jsDate);
-  console.log("date: " + date);
   return DateTime.fromJSDate(date).toLocaleString(DateTime.DATETIME_MED);
 };
